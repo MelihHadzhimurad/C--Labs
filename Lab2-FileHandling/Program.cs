c0f2c6c2d854aa4ac03b3cf2bbba8a70dbbdcbd3
melihHadzhimurad <melihchohacimurat10@gmail.com>
2024-11-11 22:32:24 +0200

    Read from txt, then map to json

--- Lab2-FileHandling/Program.cs
+++ Lab2-FileHandling/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace Lab2_FileHandling
+﻿using System.Runtime.InteropServices;
+using System.Text.Json;
+
+namespace Lab2_FileHandling
 {
-    internal class Program
+    public class Program
     {
+        struct GeoLocation
+        {
+            public float Lat { get; set; }
+            public float Lng { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string rawContent = File.ReadAllText("../input-01.txt");
+
+            List<GeoLocation> locations = new List<GeoLocation>();
+
+            foreach (string record in rawContent.Split(";"))
+            {
+                string[] geoPoints = record.Split(",");
+
+                locations.Add
+                (
+                    new GeoLocation
+                    {
+                        Lat = float.Parse(geoPoints[0], System.Globalization.CultureInfo.InvariantCulture),
+                        Lng = float.Parse(geoPoints[1], System.Globalization.CultureInfo.InvariantCulture)
+                    }
+                );
+            }
+
+            File.WriteAllText("../jsonContent.txt", JsonSerializer.Serialize(locations));
         }
     }
 }
