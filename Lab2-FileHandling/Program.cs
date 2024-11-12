bb867aaa0dc4a0bf4635299b4f48a697c7b92b66
melihHadzhimurad <melihchohacimurat10@gmail.com>
2024-11-12 12:00:02 +0200

    Add converting to csv in order to see points in google mymaps

--- Lab2-FileHandling/Program.cs
+++ Lab2-FileHandling/Program.cs
@@ -5,33 +5,20 @@ namespace Lab2_FileHandling
 {
     public class Program
     {
-        struct GeoLocation
-        {
-            public float Lat { get; set; }
-            public float Lng { get; set; }
-        }
-
         static void Main(string[] args)
         {
             string rawContent = File.ReadAllText("../input-01.txt");
 
-            List<GeoLocation> locations = new List<GeoLocation>();
+            string csvData = ProgramFeatures.ConvertDataToCsv(rawContent);
 
-            foreach (string record in rawContent.Split(";"))
+            if (File.Exists("../locationsData.csv"))
             {
-                string[] geoPoints = record.Split(",");
-
-                locations.Add
-                (
-                    new GeoLocation
-                    {
-                        Lat = float.Parse(geoPoints[0], System.Globalization.CultureInfo.InvariantCulture),
-                        Lng = float.Parse(geoPoints[1], System.Globalization.CultureInfo.InvariantCulture)
-                    }
-                );
+                File.AppendAllText("../locationsData.csv", csvData);
             }
-
-            File.WriteAllText("../jsonContent.txt", JsonSerializer.Serialize(locations));
+            else
+            {
+                File.WriteAllText("../locationsData.csv", "Name,Latitude,Longtitude,\n" + csvData);
+            }
         }
     }
 }
---
+++ Lab2-FileHandling/ProgramFeatures.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_FileHandling
+{
+    public static class ProgramFeatures
+    {
+        public static string ConvertDataToCsv(string rawContent)
+        {
+            string csvData = string.Empty;
+            int counter = 1;
+
+            foreach (string record in rawContent.Split(";"))
+            {
+                string[] geoPoints = record.Split(",");
+
+                if (geoPoints.Length > 2)
+                {
+                    csvData += $"{geoPoints[0]},{geoPoints[1]},{geoPoints[2]},\n";
+                }
+                else
+                {
+                    csvData += $"Location{counter},{geoPoints[0]},{geoPoints[1]},\n";
+                    counter++;
+                }
+            }
+
+            return csvData;
+        }
+
+        public static string ConvertDatatoJson(string rawData)
+        {
+            return string.Empty;
+        }
+    }
+}
