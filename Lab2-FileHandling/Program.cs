b5808f36836fdc67609e9f24522f3e851227593d
melihHadzhimurad <melihchohacimurat10@gmail.com>
2024-11-12 19:03:33 +0200

    Add Contacts data handling functionality

--- Lab2-FileHandling/Program.cs
+++ Lab2-FileHandling/Program.cs
@@ -1,24 +1,112 @@
-﻿using System.Runtime.InteropServices;
-using System.Text.Json;
-
-namespace Lab2_FileHandling
+﻿namespace Lab2_FileHandling
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            string rawContent = File.ReadAllText("../input-01.txt");
+            int choice = 0;
 
-            string csvData = ProgramFeatures.ConvertDataToCsv(rawContent);
+            do
+            {
+                Console.WriteLine("To convert data from txt to csv press - 1\nTo get contacts from txt press - 2\nTo exit the program press - 0");
 
-            if (File.Exists("../locationsData.csv"))
-            {
-                File.AppendAllText("../locationsData.csv", csvData);
+                try
+                {
+                    choice = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Make choice, please!");
+                    continue;
+                }
+
+                switch (choice)
+                {
+                    case 0:
+                        break;
+
+                    case 1:
+                        string rawContent = File.ReadAllText("../input-01.txt");
+
+                        string csvData = ProgramFeatures.ConvertDataToCsv(rawContent);
+
+                        if (File.Exists("../locationsData.csv"))
+                        {
+                            File.AppendAllText("../locationsData.csv", csvData);
+                        }
+                        else
+                        {
+                            File.WriteAllText("../locationsData.csv", "Name,Latitude,Longtitude,\n" + csvData);
+                        }
+                        break;
+
+                    case 2:
+                        string rawData = File.ReadAllText("../input-02.txt");
+
+                        List<Contact> contacts = ProgramFeatures.ReadContactsFromFile(rawData);
+
+                        foreach (Contact contact in contacts)
+                        {
+                            Console.WriteLine(contact.ShowDetails());
+                        }
+
+                        Console.WriteLine("Do you want to save contacts as XML? (Y/N)");
+                        string saveXmlChoice = Console.ReadLine();
+
+                        switch (saveXmlChoice.ToUpper())
+                        {
+                            case "Y":
+                                ProgramFeatures.SaveContactsAsXml(contacts);
+                                Console.WriteLine("The data has been saved successfully!");
+                                break;
+
+                            case "N":
+                                break;
+
+                            default:
+                                break;
+                        }
+
+                        Console.WriteLine("Do you want to get specific contact? (Y/N)");
+                        string serchContactChoice = Console.ReadLine();
+
+                        switch (serchContactChoice.ToUpper())
+                        {
+                            case "Y":
+
+                                Console.WriteLine("Please enter the field to search for: (Identifier/Name)");
+                                string searchKey = Console.ReadLine();
+
+                                try
+                                {
+                                    foreach (Contact contact in ProgramFeatures.getExactContact(searchKey, contacts))
+                                    {
+                                        Console.WriteLine(contact.ShowDetails());
+                                    }
+                                }
+                                catch (KeyNotFoundException)
+                                {
+                                    Console.WriteLine("Contact with specified field has not been found!");
+                                }
+
+                                break;
+
+                            case "N":
+                                break;
+
+                            default:
+                                break;
+                        }
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Unsupported operation!");
+                        break;
+                }
             }
-            else
-            {
-                File.WriteAllText("../locationsData.csv", "Name,Latitude,Longtitude,\n" + csvData);
-            }
+            while (choice != 0);
+
         }
     }
 }
--- Lab2-FileHandling/ProgramFeatures.cs
+++ Lab2-FileHandling/ProgramFeatures.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Xml;
 
 namespace Lab2_FileHandling
 {
@@ -34,6 +31,116 @@ namespace Lab2_FileHandling
         public static string ConvertDatatoJson(string rawData)
         {
             return string.Empty;
+        }
+
+        public static List<Contact> ReadContactsFromFile(string rawData)
+        {
+            string[] cleanData = rawData.Split([" ", "\t", "\r", "\n"], StringSplitOptions.RemoveEmptyEntries);
+
+            List<Contact> contacts = new List<Contact>();
+            int counter = 0;
+            string name = string.Empty;
+            string number = string.Empty;
+            string id = string.Empty;
+
+            foreach (var item in cleanData)
+            {
+                if (counter == 3 && number.Length == 14)
+                {
+                    contacts.Add(new Contact(id, name, number));
+                    counter = 0;
+                }
+
+                if (item.StartsWith("+"))
+                {
+                    number = item;
+                    counter++;
+                    continue;
+                }
+
+                if (char.IsDigit(item[0]))
+                {
+                    if (item.Length > 3)
+                    {
+                        id = item;
+                        counter++;
+                        continue;
+                    }
+                    else
+                    {
+                        number += $" {item}";
+                        continue;
+                    }
+                }
+                else
+                {
+                    name = item;
+                    counter++;
+                    continue;
+                }
+            }
+
+            return contacts;
+        }
+
+        public static void SaveContactsAsXml(List<Contact> contacts)
+        {
+            XmlDocument document = new XmlDocument();
+
+            XmlNode rootElement = document.CreateElement("contacts");
+
+            foreach (var item in contacts)
+            {
+                XmlElement nameTag = document.CreateElement("name");
+                nameTag.InnerText = item.Name;
+
+                XmlElement numberTag = document.CreateElement("number");
+                numberTag.InnerText = item.Number;
+
+                XmlElement contactNode = document.CreateElement("contact");
+                contactNode.SetAttribute("id", item.Id);
+
+                contactNode.AppendChild(nameTag);
+                contactNode.AppendChild(numberTag);
+
+                rootElement.AppendChild(contactNode);
+            }
+
+            document.AppendChild(rootElement);
+
+            document.Save("../contacts.xml");
+        }
+
+        public static List<Contact> getExactContact(string identifier, List<Contact> contacts)
+        {
+            List<Contact> filteredContacts;
+
+            if (char.IsDigit(identifier[0]))
+            {
+                filteredContacts = contacts.FindAll(contact => contact.Id.Equals(identifier));
+
+                if (filteredContacts.Count == 0)
+                {
+                    throw new KeyNotFoundException();
+                }
+                else
+                {
+                    return filteredContacts;
+                }
+            }
+            else
+            {
+                filteredContacts = contacts.FindAll(contact => contact.Name.ToLower().Equals(identifier.ToLower()));
+
+                if (filteredContacts.Count == 0)
+                {
+                    throw new KeyNotFoundException();
+                }
+                else
+                {
+                    return filteredContacts;
+                }
+            }
         }
     }
 }
