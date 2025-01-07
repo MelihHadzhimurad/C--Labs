09e78eac85377f08ecd1608ffb9b9779ad9cd695
melihHadzhimurad <melihchohacimurat10@gmail.com>
2025-01-07 15:31:13 +0200

    Completed tasks from Lab1

---
+++ Lab1-FeaturesOfTheLanguage/Circle.cs
@@ -0,0 +1,27 @@
+﻿namespace Lab1_FeaturesOfTheLanguage
+{
+    public class Circle : Shape, ICheckForElipse
+    {
+        private float _radius;
+
+        public Circle(float radius)
+        {
+            _radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return 2*(3.14*(_radius * _radius));
+        }
+
+        public override double GetPerimeter()
+        {
+            return 2 * (3.14 * _radius);
+        }
+
+        public bool IsElipse()
+        {
+            return true;
+        }
+    }
+}
---
+++ Lab1-FeaturesOfTheLanguage/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿namespace Lab1_FeaturesOfTheLanguage
+{
+    public class Rectangle : Shape, ICheckForElipse
+    {
+        private float _longSide;
+        private float _shortSide;
+
+        public Rectangle(float shortSide, float longSide)
+        {
+            _longSide = longSide;
+            _shortSide = shortSide;
+        }
+
+        public override double GetArea()
+        {
+            return (_longSide * _shortSide);
+        }
+
+        public override double GetPerimeter()
+        {
+            return ((_longSide * 2) + (_shortSide * 2));
+        }
+
+        public bool IsElipse()
+        {
+            return false;
+        }
+    }
+}
